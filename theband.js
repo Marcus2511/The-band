4b230a78f94cb2be84719e6d6c84a3573e61565e
github_Marcus2511 <ratofhamelin1125@gmail.com>
2023-02-02 18:28:29 +0700

    initial commit

--- theband.js
+++ theband.js
@@ -3,6 +3,22 @@ var overlay = document.querySelector(".overlays");
 var button = document.querySelectorAll(".place-buy");
 var modal = document.querySelector(".modal");
 var closebtn = document.querySelector(".modal-footer button");
+let slideIndex = 0;
+
+const showSlides = () => {
+  let a;
+  let slides = document.getElementsByClassName("skeleton");
+  console.log(slides);
+  for (a = 0; a < slides.length; a++) {
+    slides[a].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000);
+};
 
 const closed = () => {
   overlay.classList.add("hide");
@@ -17,3 +33,4 @@ closebtn.addEventListener("click", closed);
 for (var i = 0; i < 3; i++) {
   button[i].addEventListener("click", buy);
 }
+showSlides();
